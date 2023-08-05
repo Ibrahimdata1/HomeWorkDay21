e1b07774bc91219b3b9d4df40246436964862870
Ibrahimdata1 <bsk.kosit1@gmail.com>
2023-08-05 08:27:50 +0700

    saved1 finished input

---
+++ myReact/src/App.tsx
@@ -0,0 +1,13 @@
+import My_Form from './Components/My_Form'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <My_Form/>
+    </>
+  )
+}
+
+export default App
---
+++ myReact/src/Components/My_Form.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+const My_Form =()=>{
+    const [Weight,setWeight] = useState<number>(0)
+    const [result,setResult] = useState<number>()
+    const clickWeight = (event: React.ChangeEvent<HTMLInputElement>)=>{
+        setWeight(+event.target.value)
+    }
+    const CalResult =()=>{
+            setResult(Weight*2.2*30/2)
+    }
+    return(
+        <>
+            <h1>ควรดื่มน้ำวันละเท่าไร???</h1>
+            <h3>คำตอบคือ: {result} ML.</h3>
+            <input type="number" onChange={clickWeight} placeholder="Insert Bodyweight(KG)" className="inputCSS"/>
+            <button onClick={CalResult}>Calculate</button>
+        </>
+    )
+}
+export default My_Form
